0589c7bdb296c8a4e0118bc5ccba20364a896c8d
tqkoh <shktmygs@gmail.com>
2022-08-07 22:11:24 +0900

    deploy test

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-import os;
+use std::env;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
 
 fn main() {
-    let listener = TcpListener::bind(os.getEnv("NARO3_URL")).unwrap();
+    let listener = TcpListener::bind(match env::var("NARO3_URL"){
+			Ok(val)=>val,
+			Err(_e)=>String::from("127.0.0.1:8080")
+		}).unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
