134c801442969566dfc166f5495b1baabac47895
tqkoh <shktmygs@gmail.com>
2022-08-07 16:06:56 +0900

    deploy test
    release build にした
    バイナリを置くようにした

---
+++ target/release/deps/naro3-e3feecaa4b1a2f82.d
@@ -0,0 +1,5 @@
+/home/tqk/repos/naro3/target/release/deps/naro3-e3feecaa4b1a2f82: src/main.rs
+
+/home/tqk/repos/naro3/target/release/deps/naro3-e3feecaa4b1a2f82.d: src/main.rs
+
+src/main.rs:
---
+++ target/release/naro3.d
@@ -0,0 +1,1 @@
+/home/tqk/repos/naro3/target/release/naro3: /home/tqk/repos/naro3/src/main.rs
