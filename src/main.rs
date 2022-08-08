b385765afeb6146003bf460236f2b635f7a723fd
tqkoh <shktmygs@gmail.com>
2022-08-08 10:56:29 +0900

    use env

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,31 @@
+use std::env;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
 
 fn main() {
-    let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
+  let address = match env::var("NARO3_ADDRESS"){
+    Ok(val) => val,
+    Err(_e) => "localhost:8080".to_string()
+  };
+  let listener = TcpListener::bind(address).unwrap();
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
+  for stream in listener.incoming() {
+    let stream = stream.unwrap();
 
-        handle_connection(stream);
-    }
+    handle_connection(stream);
+  }
 }
 
 fn handle_connection(mut stream: TcpStream) {
-	let mut buffer = [0; 1024];
-	stream.read(&mut buffer).unwrap();
-	println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+  let mut buffer = [0; 1024];
+  stream.read(&mut buffer).unwrap();
+  println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
-	let contents = "Hello, World!";
+  let contents = "Hello, World!";
 
-	let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+  let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
 
-	stream.write(response.as_bytes()).unwrap();
-	stream.flush().unwrap();
+  stream.write(response.as_bytes()).unwrap();
+  stream.flush().unwrap();
 }
