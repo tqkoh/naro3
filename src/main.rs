93fe4e55e49c55da8370ca9aafeeb0008bd1fb7f
tqkoh <shktmygs@gmail.com>
2022-08-08 11:51:53 +0900

    use actix-web

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,25 @@
-use std::env;
-use std::io::prelude::*;
-use std::net::TcpStream;
-use std::net::TcpListener;
+use actix_web::{middleware, web, App, HttpRequest, HttpServer};
 
-fn main() {
-  let address = match env::var("NARO3_ADDRESS"){
-    Ok(val) => val,
-    Err(_e) => "localhost:8080".to_string()
-  };
-  let listener = TcpListener::bind(address).unwrap();
-
-  for stream in listener.incoming() {
-    let stream = stream.unwrap();
-
-    handle_connection(stream);
-  }
+async fn index(req: HttpRequest) -> &'static str {
+  println!("Request: {req:?}");
+  "Hello, World!"
 }
 
-fn handle_connection(mut stream: TcpStream) {
-  let mut buffer = [0; 1024];
-  stream.read(&mut buffer).unwrap();
-  println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+  std::env::set_var("RUST_LOG", "actix_web=info");
+  env_logger::init();
 
-  let contents = "Hello, World!";
-
-  let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-
-  stream.write(response.as_bytes()).unwrap();
-  stream.flush().unwrap();
+  let address = match std::env::var("NARO3_ADDRESS"){
+    Ok(val) => val,
+    Err(_e) => "localhost".to_string()
+  };
+  HttpServer::new(|| {
+    App::new()
+      .wrap(middleware::Logger::default())
+      .service(web::resource("/").to(index))
+  })
+  .bind((address, 8080))?
+  .run()
+  .await
 }
