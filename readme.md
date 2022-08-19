949a00480496c178e71e0b087f84c941b250303e
tqkoh <shktmygs@gmail.com>
2022-08-19 12:20:00 +0900

    update signup, login

--- readme.md
+++ readme.md
@@ -9,11 +9,54 @@ http://naro3.tqk.trap.show/
 - `GET /` : "Hello, World!"
 - `GET /ping`: "pong"
 - `GET /hello/{name}` : "Hello, {name}!"
-- `GET /fizzbuzz?count={count}` : 0 より大きく count までの fizzbuzz
+- `POST /signup`: 登録
+```
+{
+    "username": "tqk",
+    "password": "wMQnbeznUn8N"
+}
+```
+- `POST /login`: ログイン
+```
+{
+    "username": "tqk",
+    "password": "wMQnbeznUn8N"
+}
+```
+- `GET /hello`: "Hello, {username}!", ログインしていればそのユーザーネーム、していなければゲスト
+- `GET /fizzbuzz?count={count}` : [1, count] の fizzbuzz
 - `GET /city/{name}`: 名前が name の都市の情報
 - `POST /post` :
-  - json のうちから id: int, name: string, arr: array<int> を受け取り、それらのみの json
+  - 一部切り取ってそのまま返す 意味はない arr: [int]
+```
+{
+    "id": 123,
+    "name": "ddd",
+		"arr": [998244353,998244353,998244353,998244353,-1]
+}
+```
 - `POST /add` :
-  - json から left: int, right: int 受け取って、和 answer: int だけを入れた json
+  - 足して返す
+```
+{
+    "left": 123,
+    "right": 678
+}
+```
+```
+```
+{
+    "answer": 801
+}
+```
+```
 - `POST /postcity`:
   - json から name: string, countryCode: string, district,: string, population: int 受け取って、データベースに追加する
+```
+{
+  	"name": "Ooookayama",
+    "country_code": "JPN",
+    "district": "Tokyo",
+    "population": 998244353
+}
+```
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use actix_identity::Identity;
+use actix_identity::{CookieIdentityPolicy, IdentityService};
 use actix_session::Session;
 use actix_web::web::Data;
 use actix_web::{
     get, middleware, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder,
 };
 use bcrypt::{hash, verify, DEFAULT_COST};
+use rand::Rng;
 use serde::{Deserialize, Serialize};
 use std::env;
 use std::sync::*;
@@ -20,6 +23,7 @@ async fn ping() -> impl Responder {
 }
 
 #[derive(Default, Serialize)]
+#[allow(non_snake_case)]
 struct City {
     ID: i32,
     Name: String,
@@ -29,7 +33,15 @@ struct City {
 }
 
 #[get("dbtest")]
-async fn dbtest(pool_data: web::Data<Arc<Mutex<sqlx::Pool<sqlx::MySql>>>>) -> impl Responder {
+async fn dbtest(
+    pool_data: web::Data<Arc<Mutex<sqlx::Pool<sqlx::MySql>>>>,
+    id: Identity,
+) -> impl Responder {
+    let username = id.identity().unwrap_or("".to_string());
+    if username == "" {
+        return HttpResponse::Forbidden().body("login required");
+    }
+
     println!("{:?}", pool_data);
     let pool = pool_data.lock().unwrap();
     let mut ret: Vec<String> = vec![];
@@ -57,7 +69,13 @@ async fn dbtest(pool_data: web::Data<Arc<Mutex<sqlx::Pool<sqlx::MySql>>>>) -> im
 async fn cities(
     name: web::Path<String>,
     pool_data: web::Data<Arc<Mutex<sqlx::Pool<sqlx::MySql>>>>,
+    id: Identity,
 ) -> impl Responder {
+    let username = id.identity().unwrap_or("".to_string());
+    if username == "" {
+        return HttpResponse::Forbidden().body("login required");
+    }
+
     let pool = pool_data.lock().unwrap();
     let ret = sqlx::query_as!(City, r#"SELECT * FROM city WHERE Name=?"#, name.to_string())
         .fetch_one(&*pool)
@@ -73,7 +91,7 @@ async fn cities(
 #[derive(Default, Deserialize)]
 struct PostCity {
     name: String,
-    countryCode: String,
+    country_code: String,
     district: String,
     population: i32,
 }
@@ -82,12 +100,18 @@ struct PostCity {
 async fn postcity(
     city: web::Json<PostCity>,
     pool_data: web::Data<Arc<Mutex<sqlx::Pool<sqlx::MySql>>>>,
+    id: Identity,
 ) -> impl Responder {
+    let username = id.identity().unwrap_or("".to_string());
+    if username == "" {
+        return HttpResponse::Forbidden().body("login required");
+    }
+
     let pool = pool_data.lock().unwrap();
     sqlx::query!(
         "INSERT INTO city (Name, CountryCode, District, Population) VALUES (?, ?, ?, ?);",
         city.name,
-        city.countryCode,
+        city.country_code,
         city.district,
         city.population,
     )
@@ -103,7 +127,12 @@ struct FizzbuzzQuery {
 }
 
 #[get("fizzbuzz")]
-async fn fizzbuzz(info: web::Query<FizzbuzzQuery>) -> impl Responder {
+async fn fizzbuzz(info: web::Query<FizzbuzzQuery>, id: Identity) -> impl Responder {
+    let username = id.identity().unwrap_or("".to_string());
+    if username == "" {
+        return HttpResponse::Forbidden().body("login required");
+    }
+
     println!("{}", info.count);
 
     if info.count < 0 {
@@ -125,8 +154,16 @@ async fn fizzbuzz(info: web::Query<FizzbuzzQuery>) -> impl Responder {
     HttpResponse::Ok().body(ret)
 }
 
+#[get("/hello")]
+async fn hello(id: Identity) -> impl Responder {
+    HttpResponse::Ok().body(format!(
+        "Hello, {}!",
+        id.identity().unwrap_or("guest".to_owned())
+    ))
+}
+
 #[get("/hello/{name}")]
-async fn hello(name: web::Path<String>) -> impl Responder {
+async fn hello_name(name: web::Path<String>) -> impl Responder {
     HttpResponse::Ok().body(format!("Hello, {name}!"))
 }
 
@@ -138,7 +175,12 @@ struct JsonData {
 }
 
 #[post("/post")]
-async fn post(info: web::Json<JsonData>) -> impl Responder {
+async fn post(info: web::Json<JsonData>, id: Identity) -> impl Responder {
+    let username = id.identity().unwrap_or("".to_string());
+    if username == "" {
+        return HttpResponse::Forbidden().body("login required");
+    }
+
     HttpResponse::Ok().json(info)
 }
 
@@ -154,7 +196,12 @@ struct AnswerJsonData {
 }
 
 #[post("/add")]
-async fn add(info: web::Json<AddJsonData>) -> impl Responder {
+async fn add(info: web::Json<AddJsonData>, id: Identity) -> impl Responder {
+    let username = id.identity().unwrap_or("".to_string());
+    if username == "" {
+        return HttpResponse::Forbidden().body("login required");
+    }
+
     let ret = AnswerJsonData {
         answer: info.left + info.right,
     };
@@ -168,6 +215,7 @@ struct LoginRequest {
 }
 
 #[derive(Default)]
+#[allow(non_snake_case)]
 struct User {
     Username: String,
     HashedPass: String,
@@ -182,7 +230,7 @@ async fn signup(
     if req.username == "" || req.password == "" {
         return HttpResponse::BadRequest().body("username or password cannot be empty");
     }
-    let hashed_pass = hash(req.password, DEFAULT_COST).unwrap();
+    let hashed_pass = hash(&req.password, DEFAULT_COST).unwrap();
     let count = sqlx::query!(
         r#"SELECT COUNT(*) as value FROM users WHERE Username=?"#,
         req.username
@@ -193,7 +241,8 @@ async fn signup(
     .value;
 
     if count > 0 {
-        return HttpResponse::Conflict().body("user already exists");
+        return HttpResponse::Conflict()
+            .body(format!("user {} already exists", req.username.to_owned()));
     }
 
     sqlx::query!(
@@ -212,7 +261,7 @@ async fn signup(
 async fn login(
     req: web::Json<LoginRequest>,
     pool_data: web::Data<Arc<Mutex<sqlx::Pool<sqlx::MySql>>>>,
-    session: Session,
+    id: Identity,
 ) -> impl Responder {
     let pool = pool_data.lock().unwrap();
     let user = sqlx::query_as!(User, "SELECT * FROM users WHERE Username = ?", req.username)
@@ -225,8 +274,10 @@ async fn login(
         return HttpResponse::Forbidden().body("password does not match");
     }
 
-    session.insert("username", &req.username);
-    HttpResponse::Ok().body("login successful")
+    id.remember(req.username.to_owned());
+    HttpResponse::Found()
+        .insert_header(("location", "/hello"))
+        .finish()
 }
 
 #[actix_web::main]
@@ -258,21 +309,28 @@ async fn main() -> std::io::Result<()> {
         .unwrap();
 
     let pool_data = Arc::new(Mutex::new(pool));
+    let private_key = rand::thread_rng().gen::<[u8; 32]>();
     HttpServer::new(move || {
         App::new()
-            .wrap(middleware::Logger::default())
             .app_data(Data::new(pool_data.clone()))
+            .wrap(IdentityService::new(
+                CookieIdentityPolicy::new(&private_key)
+                    .name("auth")
+                    .secure(false),
+            ))
+            .wrap(middleware::Logger::default())
             .service(index)
+            .service(signup)
+            .service(login)
             .service(ping)
             .service(fizzbuzz)
             .service(hello)
+            .service(hello_name)
             .service(post)
             .service(add)
             .service(dbtest)
             .service(postcity)
             .service(cities)
-            .service(signup)
-            .service(login)
     })
     .bind((address, 8080))?
     .run()
