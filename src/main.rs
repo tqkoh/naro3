8ec0d33141bc8661c147065aa47b40e28f351c5f
tqkoh <shktmygs@gmail.com>
2022-08-26 16:39:25 +0900

    cors test

--- src/main.rs
+++ src/main.rs
@@ -316,15 +316,19 @@ async fn main() -> std::io::Result<()> {
     let private_key = rand::thread_rng().gen::<[u8; 32]>();
     HttpServer::new(move || {
         let cors = Cors::default()
-            .allowed_origin("https://tqk.blue/")
+            .allowed_origin("https://tqk.blue")
             .max_age(3600);
         App::new()
             .app_data(Data::new(pool_data.clone()))
-            .wrap(cors)
+            .wrap(
+                middleware::DefaultHeaders::new()
+                    .add(("Access-Control-Allow-Origin", "https://tqk.blue/")),
+            )
+            //.wrap(cors)
             .wrap(IdentityService::new(
                 CookieIdentityPolicy::new(&private_key)
                     .name("auth")
-                    .secure(false),
+                    .secure(true),
             ))
             .wrap(middleware::Logger::default())
             .service(index)
